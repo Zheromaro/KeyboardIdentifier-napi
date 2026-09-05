cbeb4b741a5bbb5be6156603edd62ae5329c33a4
Dib Zahradin <zahradin.dib@gmail.com>
2026-09-05 14:30:55 +0100

    init: keyboard_identifier for node.js

---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+#![deny(clippy::all)]
+use keyboard_identifier::{keyboard_source::*, *};
+use napi::bindgen_prelude::*;
+use napi::threadsafe_function::ThreadsafeFunctionCallMode;
+use napi_derive::napi;
+use std::sync::Arc;
+
+#[napi(object)]
+pub struct JsPortId {
+  pub physical_path: Option<String>,
+}
+
+#[napi(object)]
+pub struct JsKeyboardId {
+  pub name: Option<String>,
+  pub vendor_id: Option<String>,
+  pub product_id: Option<String>,
+  pub serial: Option<String>,
+}
+
+#[napi(object)]
+pub struct JsKeyboard {
+  pub keyboard_id: JsKeyboardId,
+  pub port_id: JsPortId,
+}
+
+impl From<&Keyboard> for JsKeyboard {
+  fn from(kb: &Keyboard) -> Self {
+    JsKeyboard {
+      keyboard_id: JsKeyboardId {
+        name: kb.keyboard_id.name.clone(),
+        vendor_id: kb.keyboard_id.vendor_id.clone(),
+        product_id: kb.keyboard_id.product_id.clone(),
+        serial: kb.keyboard_id.serial.clone(),
+      },
+      port_id: JsPortId {
+        physical_path: kb.port_id.physical_path.clone(),
+      },
+    }
+  }
+}
+
+#[napi]
+impl KeyboardManager {
+  #[napi]
+  pub fn on_plugged(
+    &self,
+    #[napi(ts_arg_type = "(kb: JsKeyboard) => void")] callback: Function<JsKeyboard, ()>,
+  ) -> Result<()> {
+    let tsfn = callback
+      .build_threadsafe_function()
+      .callee_handled::<false>()
+      .build()?;
+
+    self.listener.on_plugged(move |kb| {
+      tsfn.call(kb.into(), ThreadsafeFunctionCallMode::NonBlocking);
+    });
+
+    Ok(())
+  }
+
+  #[napi]
+  pub fn on_unplugged(
+    &self,
+    #[napi(ts_arg_type = "(kb: JsKeyboard) => void")] callback: Function<JsKeyboard, ()>,
+  ) -> Result<()> {
+    let tsfn = callback
+      .build_threadsafe_function()
+      .callee_handled::<false>()
+      .build()?;
+
+    self.listener.on_unplugged(move |kb| {
+      tsfn.call(kb.into(), ThreadsafeFunctionCallMode::NonBlocking);
+    });
+
+    Ok(())
+  }
+
+  #[napi]
+  pub fn on_pressed(
+    &self,
+    #[napi(ts_arg_type = "(kb: JsKeyboard) => void")] callback: Function<JsKeyboard, ()>,
+  ) -> Result<()> {
+    let tsfn = callback
+      .build_threadsafe_function()
+      .callee_handled::<false>()
+      .build()?;
+
+    self.listener.on_pressed(move |kb| {
+      tsfn.call(kb.into(), ThreadsafeFunctionCallMode::NonBlocking);
+    });
+
+    Ok(())
+  }
+
+  #[napi]
+  pub fn get_keyboards(&self) -> Vec<JsKeyboard> {
+    self
+      .source
+      .get_keyboards()
+      .iter()
+      .map(JsKeyboard::from)
+      .collect()
+  }
+}
+
+#[napi]
+pub async fn new_keyboard_source() -> Result<KeyboardManager> {
+  let source = Arc::new(
+    OSKeyboardSource::new()
+      .await
+      .map_err(|err| Error::from_reason(err.to_string()))?,
+  );
+
+  let listener = Arc::new(KeyboardListener::new());
+
+  listener.listen(source.clone()).await;
+
+  Ok(KeyboardManager { listener, source })
+}
